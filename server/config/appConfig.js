d3ab5d712f6ff5b457afd309df359b528d724320
Julien MOULIS <julien@macbook-pro-de-julien-2.home>
2019-04-04 10:10:56 +0200

    Add config

---
+++ server/config/appConfig.js
@@ -0,0 +1,89 @@
+module.exports = {
+  entities: {
+    plants: {
+      label: 'plants',
+      entity: 'Plants',
+      displayField: 'Name',
+      uri: '/api/v1/plants',
+      initMenu: true,
+    },
+    products: {
+      label: 'products',
+      entity: 'Products',
+      displayField: 'Name',
+      uri: '/api/v1/products',
+      initMenu: true,
+    },
+    posts: {
+      label: 'posts',
+      entity: 'Posts',
+      displayField: 'OrderID',
+      uri: '/api/v1/posts',
+    },
+  },
+  router: {
+    hedging: {
+      title: 'Hedging',
+      allowed: true,
+      path: '/hedging',
+      component: 'Hedging',
+      componentPath: 'Hedging/Hedging',
+      routes: [
+        {
+          title: 'Plants',
+          path: '/hedging/plants',
+          uri: '/api/v1/plants',
+          component: 'HedgeContent',
+          componentPath: 'Hedging/HedgeContent',
+          routes: [
+            {
+              title: 'Plants',
+              path: '/:id',
+              component: 'HedgeContent',
+              componentPath: 'Hedging/HedgeContent',
+            },
+          ],
+        },
+        {
+          title: 'Products',
+          path: '/hedging/products',
+          uri: '/products.json',
+          component: 'HedgeContent',
+          componentPath: 'Hedging/HedgeContent',
+        },
+        {
+          title: 'Posts',
+          path: '/hedging/plants/posts',
+          component: 'HedgeContent',
+          componentPath: 'Hedging/HedgeContent',
+          uri: '/plantsProductsPosts.json',
+        },
+      ],
+    },
+    category: {
+      title: 'Exo 1',
+      allowed: true,
+      path: '/category',
+      component: 'Categories',
+      componentPath: 'Categories/Category',
+      routes: [
+        {
+          path: '/:category',
+          title: 'Exercice 1',
+          allowed: true,
+          component: 'CategoryDetail',
+          componentPath: 'Categories/CategoryDetail/CategoryDetail',
+        },
+      ],
+    },
+    app3: {
+      title: 'App 3',
+      allowed: false,
+      exact: true,
+      path: '/app3',
+      component: 'Appli3',
+      routes: [],
+    },
+  },
+  initNavMenu: ['plants', 'products'],
+};
